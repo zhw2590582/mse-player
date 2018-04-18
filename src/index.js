63d6fb0214346441412c113d6da8e6b314c8a39f
zhw2590582 <laozhaochaguan@gmail.com>
2018-04-18 17:48:29 +0800

    add file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-[].find(i => i);
+import Emitter from 'tiny-emitter';
+
+class MsePlayer extends Emitter {
+  constructor(options) {
+    super();
+    this.options = {
+      ...MsePlayer.DEFAULTS,
+      ...options
+    };
+
+    this._init();
+  }
+
+  static get DEFAULTS() {
+    return {
+
+    };
+  }
+
+  _init() {
+    console.log(this)
+  }
+}
+
+window.MsePlayer = MsePlayer;
+export default MsePlayer;
